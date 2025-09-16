6c23c4f95d270a1cfc94a88df943299451cba694
Muqsit <notmuqsit+a4i@gmail.com>
2025-09-16 21:12:56 +0800

    Update index.py

--- python/flask3/api/index.py
+++ python/flask3/api/index.py
@@ -1,6 +1,16 @@
 from flask import Flask
 
+data = 0
 app = Flask(__name__)
+
+@app.route("/get")
+def get():
+    return str(data)
+
+@app.route("/set")
+def set():
+    data += 1
+    return "Done"
 
 @app.route('/')
 def home():
